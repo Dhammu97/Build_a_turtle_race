9873370a82b14787206ded81b2224d3bc46f049f
Dhammu97 <dhammdipmodak596@gmail.com>
2024-03-29 00:17:39 +0530

    This is a main file of turtle race game

--- participant.py
+++ participant.py
@@ -3,18 +3,21 @@ import random
 
 COLOR = ["red", "green", "blue", "yellow", "orange", "purple"]
 Y_POSITION = [-70, -40, -10, 20, 50, 80]
-all_turtle = []
+ 
 
 
 class participant:
-
+    all_turtle = []
     def __init__(self):
-        
+
         for turtle_index in range (0, 6):
             new_turtle = Turtle("turtle")
             new_turtle.penup()
             new_turtle.color(COLOR[turtle_index])
             new_turtle.goto(x = -230, y = Y_POSITION[turtle_index])
-            all_turtle.append(new_turtle)
+            self.all_turtle.append(new_turtle)
 
-
+    # def move(self):
+    #     for turtle in all_turtle:
+    #         rand_dist = random.randint(0, 10)
+    #         turtle.forward(rand_dist)
